7f4bed4eb12cda0731bd4a6453a571c126c2b82e
LeoSanGo <souoleo@gmail.com>
2021-05-10 17:39:51 -0300

    A simple javascript exercise from codewars

---
+++ reverseword.js
@@ -0,0 +1,20 @@
+/* Complete the function that accepts a string parameter, and reverses each word in the string. 
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps" */
+
+
+function reverseWords(str) {
+    const newString = str.split(' ');
+    console.log(newString.length);
+    for (let i = 0; i < newString.length; i++) {
+        let element = newString[i];
+        console.log(element)
+        return element;
+    }
+    //return newString;
+  }
+
+  console.log(reverseWords("Testa string"));
