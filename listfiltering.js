72d352c34bf066e37e6a825401c4255cf835950e
leosango <souoleo@gmail.com>
2022-01-30 23:13:29 -0300

    update information

--- listfiltering.js
+++ listfiltering.js
@@ -51,16 +51,4 @@ function filter_list(l) {
 
 ------------------------------------------------------------
 
-function filter_list(l) {
-var res = [];
-            for(var item=0;item<l.length;item++)
-            {
-                if (typeof l[item] === "number")
-                {
-                    res.push(l[item]);
-                }
-  
-            }
-            return res;
-}
 */
