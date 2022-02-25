8c3183ceb3dba147d2bc5775922ff15ac2026f57
leosango <souoleo@gmail.com>
2022-02-24 23:15:25 -0300

    A codewars exercise

---
+++ returningstrings.js
@@ -0,0 +1,24 @@
+/* 
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+/* 
+Other solutions
+
+function greet(name){
+  return "Hello, <name> how are you doing today?".replace("<name>", name);
+}
+
+-------------------------------------------
+
+const greet = function(name){
+  const arr = ['Hello,', name,'how', 'are', 'you', 'doing', 'today?']
+  return arr.join(' ')
+}
+*/
