1e6a09e7bd751c9dd8faf91b6c477b1918adabd1
leosango <souoleo@gmail.com>
2022-03-02 23:12:52 -0300

    a codewars exercise

--- areyouplayngbanjo.js
+++ areyouplayngbanjo.js
@@ -5,3 +5,14 @@ function areYouPlayingBanjo(name) {
   return name.startsWith('R') || name.startsWith('r') ? name + ' plays banjo' : 
   name + ' does not play banjo'
 }
+
+/* 
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
+
+----------------------------------------
+
+const areYouPlayingBanjo = name => (name.toUpperCase().startsWith('R')) ? name + ' plays banjo' : name +' does not play banjo';
+
+*/
