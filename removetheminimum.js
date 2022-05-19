b09f418fb309318af9ec44f1e0bddb9feccce10f
leosango <souoleo@gmail.com>
2022-05-18 23:43:06 -0300

    A codewars exercise

--- removetheminimum.js
+++ removetheminimum.js
@@ -22,3 +22,31 @@ function removeSmallest(numbers) {
 }
 
 console.log(removeSmallest([2, 2, 1, 2, 1]));
+
+/* 
+other solutions
+
+function removeSmallest(numbers) {
+  let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
+
+function removeSmallest(numbers) {
+  const min = Math.min.apply(this, numbers);
+  return numbers.filter((num, idx, arr) => idx !== arr.indexOf(min));
+}
+
+const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
+
+
+function removeSmallest(numbers) {
+  numbers = numbers.slice(0);
+  const min = Math.min(...numbers);
+  numbers.splice(numbers.indexOf(min), 1);
+  return numbers;
+}
+
+const removeSmallest = numbers =>
+  numbers.filter((_, idx) => idx !== numbers.indexOf(Math.min(...numbers)));
+
+*/
